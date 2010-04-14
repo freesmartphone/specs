bc64dcf2c853f7d23c28d4ea36ea05caf9c25f0b
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2010-04-14 12:51:14 +0200

    tools/rules.mk: remove dead code

--- tools/rules.mk
+++ tools/rules.mk
@@ -5,46 +5,28 @@ XSLT_FLAGS = -o $@ ../tools/spec-strip-docs.xsl $<
 XSLT = xsltproc $(XSLT_FLAGS)
 
 docbook_xml_files = $(addprefix docbook-, $(files))
-docbook_xhtml_files = $(addprefix xhtml/, $(docbook_xml_files:.xml=.xhtml))
 pydoc_html_files = $(files:.xml=.html)
-
-# choose your style sheet
-#docbook_style_sheet = /usr/share/sgml/docbook/xsl-stylesheets/xhtml/docbook.xsl
-docbook_style_sheet = /usr/share/xml/docbook/stylesheet/nwalsh/xhtml/docbook.xsl
 
 all: pydocs xml
 
 xml: $(files)
 
-check:
-	@echo checking integrity...
-	@xmllint *.xml.in >/dev/null && echo "OK" || echo "FAIL"
-
-doc-dirs:
-	@mkdir -p xhtml
-
-docs: doc-dirs $(docbook_xhtml_files)
-
 pydocs: $(pydoc_html_files)
 
 $(pydoc_html_files): %.html: %.xml.in
-	@echo processing $@
+	@echo calling pydoc on $@
 	../tools/makedoc.py $< >/dev/null
 
-$(docbook_xhtml_files): xhtml/%.xhtml: %.xml
-	@echo processing $@ ...
-	@xsltproc -o $@ $(docbook_style_sheet) $<
-
-$(docbook_xml_files): docbook-%.xml: %.xml.in
-	@echo processing $@ ...
-	@xsltproc -o $@ ../tools/spec-to-docbook.xsl $<
-
 $(files): %.xml: %.xml.in
-	@echo processing $@ ...
+	@echo calling xsltproc spec-strip-docs on $@ ...
 	@xsltproc -o ../xml/$@ ../tools/spec-strip-docs.xsl $<
 
 install: xml
 	@echo nothing to install here
+
+check:
+	@echo checking integrity...
+	@xmllint *.xml.in >/dev/null && echo "OK" || echo "FAIL"
 
 clean:
 	@rm -f $(files) *~
